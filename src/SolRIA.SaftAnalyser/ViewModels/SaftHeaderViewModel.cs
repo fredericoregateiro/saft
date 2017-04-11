321e639a194c6a8af6ea689c3f1b22e0c90e25a1
Frederico Regateiro <freddregateiro@sapo.pt>
2017-04-11 01:14:05 +0100

    Add nuget updates Add Sqlite nuget packages

--- src/SolRIA.SaftAnalyser/ViewModels/SaftHeaderViewModel.cs
+++ src/SolRIA.SaftAnalyser/ViewModels/SaftHeaderViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Prism.Mvvm;
+using SolRia.Erp.MobileApp.Models.SaftV4;
+using SolRIA.SaftAnalyser.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,24 @@ namespace SolRIA.SaftAnalyser.ViewModels
 {
 	public class SaftHeaderViewModel : BindableBase
 	{
+		INavigationService navService;
+		public SaftHeaderViewModel(INavigationService navService)
+		{
+			this.navService = navService;
 
+			navService.LoadCompleted += NavService_LoadCompleted;
+		}
+
+		private void NavService_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+		{
+			Cabecalho = OpenedFileInstance.SaftFile.Header;
+		}
+
+		private Header cabecalho;
+		public Header Cabecalho
+		{
+			get { return cabecalho; }
+			set { SetProperty(ref cabecalho, value); }
+		}
 	}
 }
