2ba9466c7237f37362a17bea914d9527c1f9c2c2
Frederico Regateiro <freddregateiro@sapo.pt>
2017-04-12 22:48:43 +0100

    Do customers search

--- src/SolRIA.SaftAnalyser/ViewModels/SaftCustomersViewModel.cs
+++ src/SolRIA.SaftAnalyser/ViewModels/SaftCustomersViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using SolRia.Erp.MobileApp.Models.SaftV4;
 using SolRIA.SaftAnalyser.Interfaces;
+using System.Linq;
 
 namespace SolRIA.SaftAnalyser.ViewModels
 {
@@ -12,6 +14,8 @@ namespace SolRIA.SaftAnalyser.ViewModels
 		{
 			this.navService = navService;
 			this.messageService = messageService;
+
+			SearchCommand = new DelegateCommand(OnSearch);
 
 			navService.LoadCompleted += NavService_LoadCompleted;
 		}
@@ -29,6 +33,42 @@ namespace SolRIA.SaftAnalyser.ViewModels
 		{
 			get { return customers; }
 			set { SetProperty(ref customers, value); }
+		}
+
+		private string filter;
+		public string Filter
+		{
+			get { return filter; }
+			set { SetProperty(ref filter, value); }
+		}
+
+		//SearchCommand
+		public DelegateCommand SearchCommand { get; private set; }
+		public virtual void OnSearch()
+		{
+			FilterCustomers();
+		}
+
+		private void FilterCustomers()
+		{
+			if (Customers != null)
+			{
+				if (string.IsNullOrEmpty(filter))
+					Customers = OpenedFileInstance.Instance.SaftFile.MasterFiles.Customer;
+				else
+				{
+					Customers = (
+						from c in OpenedFileInstance.Instance.SaftFile.MasterFiles.Customer
+						where (!string.IsNullOrEmpty(c.CompanyName) && c.CompanyName.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0) ||
+						(!string.IsNullOrEmpty(c.CustomerID) && c.CustomerID.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0) ||
+						(!string.IsNullOrEmpty(c.CustomerTaxID) && c.CustomerTaxID.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0) ||
+						(!string.IsNullOrEmpty(c.Email) && c.Email.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0) ||
+						(!string.IsNullOrEmpty(c.Telephone) && c.Telephone.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0) ||
+						(!string.IsNullOrEmpty(c.Website) && c.Website.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0) ||
+						(!string.IsNullOrEmpty(c.Contact) && c.Contact.IndexOf(filter, System.StringComparison.OrdinalIgnoreCase) >= 0)
+						select c).ToArray();
+				}
+			}
 		}
 	}
 }
