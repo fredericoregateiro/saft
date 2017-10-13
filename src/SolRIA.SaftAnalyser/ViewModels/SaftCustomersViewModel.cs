837e9f12eee0ebc921e1c76e7b4feb94b475e4f1
Frederico Regateiro <freddregateiro@sapo.pt>
2017-10-13 01:02:22 +0100

    add customers export to excel

--- src/SolRIA.SaftAnalyser/ViewModels/SaftCustomersViewModel.cs
+++ src/SolRIA.SaftAnalyser/ViewModels/SaftCustomersViewModel.cs
@@ -1,7 +1,12 @@
-﻿using Prism.Commands;
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Table;
+using Prism.Commands;
 using Prism.Mvvm;
 using SolRia.Erp.MobileApp.Models.SaftV4;
 using SolRIA.SaftAnalyser.Interfaces;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace SolRIA.SaftAnalyser.ViewModels
@@ -16,8 +21,9 @@ namespace SolRIA.SaftAnalyser.ViewModels
 			this.messageService = messageService;
 
 			SearchCommand = new DelegateCommand(OnSearch);
+            DoPrintCommand = new DelegateCommand(OnPrint);
 
-			navService.LoadCompleted += NavService_LoadCompleted;
+            navService.LoadCompleted += NavService_LoadCompleted;
 		}
 
 		private void NavService_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
@@ -70,5 +76,43 @@ namespace SolRIA.SaftAnalyser.ViewModels
 				}
 			}
 		}
-	}
+
+        public DelegateCommand DoPrintCommand { get; private set; }
+        private void OnPrint()
+        {
+            if (Customers != null && Customers.Length > 0)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Código", typeof(string));
+                dt.Columns.Add("Nº conta", typeof(string));
+                dt.Columns.Add("NIF", typeof(string));
+                dt.Columns.Add("Nome", typeof(string));
+                dt.Columns.Add("Contacto", typeof(string));
+                dt.Columns.Add("Telefone", typeof(string));
+                dt.Columns.Add("Fax", typeof(string));
+                dt.Columns.Add("Email", typeof(string));
+                dt.Columns.Add("Website", typeof(string));
+                dt.Columns.Add("Autofacturação", typeof(string));
+                foreach (var c in Customers)
+                {
+                    dt.Rows.Add(c.CustomerID, c.AccountID, c.CustomerTaxID, c.CompanyName, c.Contact, c.Telephone, c.Fax, c.Email, c.Website, c.SelfBillingIndicator);
+                }
+
+                ExcelPackage pck = new ExcelPackage();
+                var wsEnum = pck.Workbook.Worksheets.Add("SolRIA | SAFT");
+                //Load the collection starting from cell A1...
+                wsEnum.Cells["A1"].LoadFromDataTable(dt, true, TableStyles.Medium9);
+                wsEnum.Cells[wsEnum.Dimension.Address].AutoFitColumns();
+
+                //...and save
+                var fi = new FileInfo(Path.GetTempPath() + Path.GetRandomFileName() + ".xlsx");
+                if (fi.Exists)
+                    fi.Delete();
+
+                pck.SaveAs(fi);
+
+                Process.Start(fi.FullName);
+            }
+        }
+    }
 }
