daa8b2e5b9fe7623329d00c4f9b8e54502c6fd12
Frederico Regateiro <freddregateiro@sapo.pt>
2017-12-20 22:03:25 +0000

    add the cli file to send the SAFT file

---
+++ src/SolRIA.SaftAnalyser/Models/FactemiCLIParams.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolRIA.SaftAnalyser.Models
+{
+    public class FactemiCLIParams
+    {
+        public int Id { get; set; }
+        public string JarPath { get; set; }
+        public string ResumePath { get; set; }
+        public string OutputPath { get; set; }
+
+        public string Command { get; set; } = " java -jar {0} -n {1} -p {2} -a {3} -m {4} -op {5} -i {6} -r {7} -o {8}";
+        public string CommandTest { get; set; } = "java -jar {0} -n {1} -p {2} -a {3} -m {4} -op {5} -i {6} -r {7} -o {8} -t";
+    }
+
+    public enum FactemiSendOption
+    {
+        enviar,
+        validar
+    }
+}
---
+++ src/SolRIA.SaftAnalyser/Views/SaftSendFile.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SolRIA.SaftAnalyser.Views
+{
+    /// <summary>
+    /// Interaction logic for SaftSendFile.xaml
+    /// </summary>
+    public partial class SaftSendFile : PageBase
+	{
+		public SaftSendFile()
+		{
+			InitializeComponent();
+        }
+    }
+}
